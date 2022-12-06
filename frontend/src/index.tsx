5defd4b0c89750a1d9408c48ac0245c9b643742c
Vic (Vu) Phan <65319838+vicphan@users.noreply.github.com>
2022-12-06 14:33:23 -0700

    completed drag and drop of tasks

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -28,9 +28,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
