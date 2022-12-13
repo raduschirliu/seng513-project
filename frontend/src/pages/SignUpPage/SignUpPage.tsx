776a80858c57ddd370a1cef6ea04942bead7c408
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-12-12 17:25:55 -0700

    grab signup data on submit

--- frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { useForm, SubmitHandler } from 'react-hook-form';
+import useAuth from '../../state/auth/useAuth';
 import "./styles.css"
 import logo from "./img/logo.png"
 
+type SignUpFormData = {
+    username: string;
+    fullName: string;
+    password: string;
+};
+
 export default function SignUpPage() {
+    const {register, handleSubmit, reset} = useForm<SignUpFormData>();
+    const {signUp} = useAuth();
+
+    const onSubmit: SubmitHandler<SignUpFormData> = (data) => {
+        console.log("Signing up in progress with\n", data);
+        reset();
+    }
 
     return (
         <div>
             <nav className="navBarContainer">
                 <h1 className="navBarTitle">Sign Up</h1>
             </nav>
+
             <section className="heroSection">
                 <div className="leftHero">
                     <img className="leftHeroImage" src={logo} alt="" />
@@ -23,13 +39,14 @@ export default function SignUpPage() {
                         <li><span>✓</span> And so much more!</li>
                     </ul>
                 </div>
-                <form className="rightHero" action="">
-                    <label className="label" htmlFor="email">Email:</label>
-                    <input className="textbox" type="text" id="email" name="email" /><br /><br />
+
+                <form onSubmit={handleSubmit(onSubmit)} className="rightHero" action="">
+                    <label className="label" htmlFor="email">Username:</label>
+                    <input {...register("username")} className="textbox" type="text" id="email" name="email" /><br /><br />
                     <label className="label" htmlFor="fullName">Full Name:</label>
-                    <input className="textbox" type="text" id="fullName" name="fullName" /><br /><br />
+                    <input {...register("fullName")} className="textbox" type="text" id="fullName" name="fullName" /><br /><br />
                     <label className="label" htmlFor="password">Password:</label>
-                    <input className="textbox" type="password" id="password" name="password" /><br /><br />
+                    <input {...register("password")} className="textbox" type="password" id="password" name="password" /><br /><br />
                     <label className="label" htmlFor="confirmPassword">Confirm Password:</label>
                     <input className="textbox" type="password" id="confirmPassword" name="confirmPassword" /><br /><br />
                     <input type="submit" defaultValue="Create" className="createButton" />
